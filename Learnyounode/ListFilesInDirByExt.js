aa9ac69a4c0e0f323089fa7f29c26eef17ca9353
Shurgu <mykolas.sauciunas@gmail.com>
2013-11-25 02:03:48 +0200

    5th lesson done

---
+++ Learnyounode/ListFilesInDirByExt.js
@@ -0,0 +1,17 @@
+var PrintOutFiles = function(err, list) {
+    if (!err) {
+        var extPatern = new RegExp('\\.' + process.argv[3] + '+$', 'i');
+        list.forEach(function (fileName) {
+            if (extPatern.test(fileName)) {
+                console.log(fileName);
+            }
+        })
+    } else {
+        console.log('Some error: ' + err);
+    }
+}
+
+if (process.argv.length > 3) {
+    var fs = require('fs');
+    fs.readdir(process.argv[2], PrintOutFiles);
+}
--- Learnyounode/NewLinesInFileASync.js
+++ Learnyounode/NewLinesInFileASync.js
@@ -1,16 +1,16 @@
 var CountLines = function(err, data) {
-	var result;
-	if (!err) {
-		var lines = data.toString().split('\n');
-		result = lines.length - 1;
-	} else {
-		result = 'Some error: ' + err;
-	}
+    var result;
+    if (!err) {
+        var lines = data.toString().split('\n');
+        result = lines.length - 1;
+    } else {
+        result = 'Some error: ' + err;
+    }
 
-	console.log(result);
+    console.log(result);
 }
 
 if (process.argv.length > 2) {
-	var fs = require('fs');
-	fs.readFile(process.argv[2], CountLines);
+    var fs = require('fs');
+    fs.readFile(process.argv[2], CountLines);
 }
